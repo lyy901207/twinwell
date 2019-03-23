e79d1d925a26b236f61ee0bb8d3697092b2f55f5
lyy901207 <lyy901207@gmail.com>
2019-03-23 23:53:25 +0900

    Update network.py
    add description

--- twinwell/model/network.py
+++ twinwell/model/network.py
@@ -8,25 +8,54 @@ class Network(object):
         self.typeGraphMap = {}
 
     def registerNode(self, node):
+        """
+        This function registers the NODE in MAP
+        :param node:
+        :return: None
+        """
         if node.id in self.idNodeMap: raise("duplicated node id")
         self.idNodeMap[node.id] = node
 
     def registerLink(self, link):
+        """
+        This function registers LINK in MAP
+        :param link:
+        :return: None
+        """
         if link.id in self.idLinkMap: raise("duplicated link id")
         self.idLinkMap[link.id] = link
 
     def registerLane(self, lane):
+        """
+        This function registers LANE in MAP
+        :param lane:
+        :return: None
+        """
         if lane.id in self.idLaneMap: raise("duplicated lane id")
         self.idLaneMap[lane.id] = lane
-        if lane.type not in self.typeGraphMap: self.typeGraphMap[lane.type] = {}
-        if lane.link.node1.id not in self.typeGraphMap[lane.type]: self.typeGraphMap[lane.type][lane.link.node1.id] = {}
+
+        if lane.type not in self.typeGraphMap:
+            self.typeGraphMap[lane.type] = {}
+        if lane.link.node1.id not in self.typeGraphMap[lane.type]:
+            self.typeGraphMap[lane.type][lane.link.node1.id] = {}
         self.typeGraphMap[lane.type][lane.link.node1.id][lane.link.node2.id] = lane
 
     def registerVehicle(self, vehicle):
+        """
+        This function registers VEHICLE in MAP
+        :param vehicle:
+        :return: None
+        """
         if vehicle.id in self.idVehicleMap: raise("duplicated vehicle id")
         self.idVehicleMap[vehicle.id] = vehicle
 
     def updateLanes(self):
+        """
+        This function update
+        1) lane.countCpu for ALL LANE;
+        2) add countCpu for vehicle.currentLane
+        :return: None
+        """
         for lane in self.idLaneMap.values():
             lane.countPcu = 0
         for vehicle in [ v for v in self.idVehicleMap.values() if v.isRunning(self.ts) ]:
@@ -36,9 +65,17 @@ class Network(object):
             lane.updatePropertiesBasedOnPcu()
 
     def runningVehicleCount(self):
+        """
+        This function counts ALL running vehicles in current time
+        :return: number of vehicle
+        """
         return len([None for v in self.idVehicleMap.values() if v.isRunning(self.ts)])
 
     def finishVehicleCount(self):
+        """
+        This function counts ALL finished vehicles in current time
+        :return: number of vehicle
+        """
         return len([None for v in self.idVehicleMap.values() if v.isFinish(self.ts)])
 
     # cross

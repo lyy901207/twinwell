9cf9e3de47ee26b9e45c74880bd1343ea59a12a3
lyy901207 <lyy901207@gmail.com>
2019-04-15 22:49:00 +0900

    Update test.py
    add sample

--- twinwell/test.py
+++ twinwell/test.py
@@ -1,8 +1,11 @@
 from util.readNetwork import *
 from util.readOd import *
 from model.network import Network
+from model.turn import *
+from lib.astar import *
+
 startTs = datetime.datetime(2019, 1, 1, 7, 0, 0)
-totalSteps = 5 #2000
+totalSteps = 20 #2000
 timeStep = 1
 
 jamDensity = 124
@@ -17,7 +20,7 @@ fNode = open("C:/Users/lyy90/OneDrive/Documents/GitHub/twinwell/twinwell/Sioux F
 fNode.readline()
 fLane = open("C:/Users/lyy90/OneDrive/Documents/GitHub/twinwell/twinwell/Sioux Falls network/lanes-SiouxFalls_gong.csv")
 fLane.readline()
-pOd = "C:/Users/lyy90/OneDrive/Documents/GitHub/twinwell/twinwell/OD_data"
+pOd = "C:/Users/lyy90/OneDrive/Documents/GitHub/twinwell/twinwell/OD_data_test"
 
 readNodes(fNode, network)
 readLanes(fLane, network)
@@ -25,6 +28,49 @@ tsPairNodePairTypeMap = readOd(pOd)
 genVehicle(tsPairNodePairTypeMap, "random", vehicleId, medianValueTime, network)
 #genVehicle(tsPairNodePairTypeMap, "uniform", vehicleId, medianValueTime, network)
 t = sorted([vehicle.startTs for vehicle in network.idVehicleMap.values()])
-print(len(t))
 
+
+for vid in network.idVehicleMap:
+    print(network.idVehicleMap[vid])
+
+networks = [network]
+
+#turn = turnGen(network)
+#sMap = stressMap(turn)
+#print(sMap)
+
+for i in range(totalSteps):
+    #print(i)
+    network = networks[-1] # current network
+
+    print(network.ts)
+
+    # update best route
+    for vid in network.idVehicleMap:
+        vehicle = network.idVehicleMap[vid]
+        if not vehicle.isRunning(network.ts): continue
+
+        # TODO: updating
+
+
+        # TODO: add new searching algorithm
+        vehicle.updateShortestPath()
+
+    # update lane features
+    network.updateLanes()
+
+    # update vehicle location
+    for vehicle in network.idVehicleMap.values():
+        if not vehicle.isRunning(network.ts): continue
+        vehicle.updateLocation(1) #update for 1 SECOND!
+
+    # decision
+
+    print(network.runningVehicleCount(), 'running')
+    print(network.finishVehicleCount(), 'finished')
+
+    # copy network to i+1
+    network_next = copy.deepcopy(network)
+    network_next.ts += datetime.timedelta(seconds=1)
+    networks.append(network_next)
 
